dd49be53f7195195791964c171dbd33d379c6177
_juliotati <59662912+Juliotati@users.noreply.github.com>
2026-02-21 18:35:03 +0000

    Add imports to provider-template.dart

--- skills/feature-structure/resources/provider-template.dart
+++ skills/feature-structure/resources/provider-template.dart
@@ -1,4 +1,5 @@
-part of '../../{{feature_name}}.dart';
+import '../../domain/repository/{{feature_name}}_repository.dart';
+import '../../data/models/models.dart';
 
 class {{feature_name_pascal}}Provider extends ChangeNotifier {
   {{feature_name_pascal}}Provider(this._repository);
