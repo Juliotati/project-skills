f9d2df9eed9d6e084dd9a16a51951650bb21d90f
_juliotati <59662912+Juliotati@users.noreply.github.com>
2026-02-21 18:31:11 +0000

    Add provider template for feature structure

---
+++ skills/feature-structure/resources/provider-template.dart
@@ -0,0 +1,7 @@
+part of '../../{{feature_name}}.dart';
+
+class {{feature_name_pascal}}Provider extends ChangeNotifier {
+  {{feature_name_pascal}}Provider(this._repository);
+
+  final {{feature_name_pascal}}Repository _repository;
+}
