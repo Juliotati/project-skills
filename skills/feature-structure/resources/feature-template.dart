85bd392545e55b10da6f43b8ce550b9ae65633b0
_juliotati <59662912+Juliotati@users.noreply.github.com>
2026-02-21 18:37:18 +0000

    Refactor feature-template.dart imports
    Updated imports to include local and remote data sources while removing unnecessary parts.

--- skills/feature-structure/resources/feature-template.dart
+++ skills/feature-structure/resources/feature-template.dart
@@ -3,13 +3,9 @@ library {{feature_name}};
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'data/models/models.dart';
+import 'presentation/providers/{{feature_name}}_provider.dart';
 
-// Domain
 part 'domain/repository/{{feature_name}}_repository.dart';
-
-// Data
-part 'data/data_sources/{{feature_name}}_remote_data_source.dart';
+part 'data/data_sources/local_data_source.dart';
+part 'data/data_sources/remote_data_source.dart';
 part 'data/repository/{{feature_name}}_repository_impl.dart';
-
-// Presentation
-part 'presentation/providers/{{feature_name}}_provider.dart';
